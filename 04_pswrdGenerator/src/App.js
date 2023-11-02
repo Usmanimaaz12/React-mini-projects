68b329e7010051bedb3b193defd5ca516c1a8c47
Maaz Usmani <usmanimaaaz12@gmail.com>
2023-11-02 22:32:40 +0530

    project 04_passwrdGenerator completed

--- 04_pswrdGenerator/src/App.js
+++ 04_pswrdGenerator/src/App.js
@@ -1,45 +1,85 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import React from 'react';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
-  const [password, setPassword] = useState(''); // useState is a hook
-  const [length, setLength] = useState(20);
-  const [character, setCharacter] = useState(true); 
-  const [numbers, setNumbers] = useState(true);
-  const [special, setSpecial] = useState(true);
+    
+  const [length, setLength] = useState(12);
+  const [numbers, setNumbers] = useState(false);
+  const [special, setSpecial] = useState(false);
+  const [password, setPassword] = useState('');
 
-  function generatePassword() {
-     let length = document.getElementById('range').value;
+  //useref variable
+  const passwordRef = useRef(null);
+
+  const copyPassToClipboard = () => {
+    passwordRef.current.select();  //select the text
+    window.navigator.clipboard.writeText(passwordRef.current.value); //copy the text
+    alert('Password copied to clipboard'); //alert the user
+
+    
+  }
+  
+const generatePassword  = useCallback(() => { 
+    let length = document.getElementById('range').value;  
     console.log(length);
     setLength(length);  
+    let pass = "";
     length = length || 20;
     let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    if (character) charset += '!@#$%^&*()';  
     if (numbers) charset += '0123456789';
-    if (special) charset += '`-=[]{};\'\\:"|,./<>?';
-    let password = '';
+    if (special) charset += '`-=[]{};\'\\:"|,./<>?!@#$%^&*()';
     for (let i = 0, n = charset.length; i < length; ++i) {
-      password += charset.charAt(Math.floor(Math.random() * n));
+      pass += charset.charAt(Math.floor(Math.random() * n));
     }
-    setPassword('password');
-  }
+    setPassword(pass);
+  }, [length, numbers, special,setPassword] );
+ 
+  useEffect( generatePassword , [length, numbers, special,generatePassword] );
 
   return (
     <>
       <div className="App">
-      <input type="text" value={password} readOnly/>
-      <button>copy</button>
-      <input type="range" min="4" max="20" name='range' id='range' onChange={generatePassword}/> 
-      <label htmlFor="range">Length: {length} </label> 
-      <h3>{password}</h3>
-      <input type="checkbox" name="character" />
-      <label htmlFor="character">Character</label>
+        <h1>Password Generator</h1>
+        <input
+          type="text"
+          value={password}
+          placeholder='Your Password will appear here'
+          readOnly
+          style={{ width: 200 }}
+          ref={passwordRef}
+        />
+        
+        <button
+          onClick={copyPassToClipboard}
+        >copy</button>
+        <input
+          type="range"
+          min="4"
+          max="25"
+          name='range'
+          id='range'
+          value={length}
+        // onChange={generatePassword}     //we have to use useEffect to make it work
+          onChange={(e)=>{setLength(e.target.value)}}
+        /> 
+        <label htmlFor="range" >Length: {length} </label> 
+        
 
-      <input type="checkbox" name="Numbers" />
+      <h3>{password}</h3>
+
+        <input
+          type="checkbox"
+          name="Numbers"
+          onClick={() => { setNumbers(!numbers) }}
+        />
       <label htmlFor="Numbers">Numbers</label>
 
-      <input type="checkbox" name="Special" />
+        <input
+          type="checkbox"
+          name="Special"
+          onClick={() => { setSpecial(!special) }} />
         <label htmlFor="Special">Special</label>
         </div>
     </>
