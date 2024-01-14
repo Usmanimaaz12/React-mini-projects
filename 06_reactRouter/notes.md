5edb3ff65cd235b814880394b2daea5b7bc0991a
Maaz Usmani <usmanimaaaz12@gmail.com>
2024-01-14 17:31:15 +0530

    React-router-dom completed Loader

--- 06_reactRouter/notes.md
+++ 06_reactRouter/notes.md
@@ -3,3 +3,10 @@
 - It is a third party library.
 - We get functionalities from installing react-router-dom. Example Link, NavLink, etc.
 - NOTE: In React we do not use <a> tag. Instead use Link tag, because it will prevent the page from relaoding.
+
+- useParams :- The useParams hook returns an object of key/value pairs of the dynamic params from the current URL that were matched by the <Route path>. Child routes inherit all params from their parent routes.
+
+- Outlet, Layout
+- Loader - optimization for api call before the mouseClick.
+- 
+
--- 06_reactRouter/src/components/Github/Github.jsx
+++ 06_reactRouter/src/components/Github/Github.jsx
@@ -5,7 +5,7 @@ function Github() {
     
     // const [data, setData] = useState([])
     // useEffect(() => {
-    //  fetch('https://api.github.com/users/hiteshchoudhary')
+    //  fetch('https://api.github.com/users/usmanimaaz12')
     //  .then(response => response.json())
     //  .then(data => {
     //     console.log(data);
@@ -28,6 +28,6 @@ export default Github
 
 
 export const githubInfoLoader = async () => {
-    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    const response = await fetch('https://api.github.com/users/Abhinav0307')
     return response.json()
 }
